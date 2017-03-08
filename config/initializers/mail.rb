c9105465dc072b7b1ba5e90d9077f87c43c88740
Jason Amerine <amerine.19@gmail.com>
2017-03-08 14:40:52 -0500

    mail config file

---
+++ config/initializers/mail.rb
@@ -0,0 +1,30 @@
+ActionMailer::Base.smtp_settings = {
+  port:              587,
+  address:           'smtp.sendgrid.net',
+  user_name:         ENV['SENDGRID_USERNAME'],
+  password:          ENV['SENDGRID_PASSWORD'],
+  domain:            'mmpr-order-form.herokuapp.herokuapp.com',
+  authentication:    :plain,
+  content_type:      'text/html'
+}
+ActionMailer::Base.delivery_method = :smtp
+
+# Makes debugging *way* easier.
+ActionMailer::Base.raise_delivery_errors = true
+
+# This interceptor just makes sure that local mail
+# only emails you.
+# http://edgeguides.rubyonrails.org/action_mailer_basics.html#intercepting-emails
+class DevelopmentMailInterceptor
+  def self.delivering_email(message)
+    message.to =  'mmpr-order-form'
+    message.cc = nil
+    message.bcc = nil
+  end
+end
+
+# Locally, outgoing mail will be 'intercepted' by the
+# above DevelopmentMailInterceptor before going out
+if Rails.env.development?
+  ActionMailer::Base.register_interceptor(DevelopmentMailInterceptor)
+end
