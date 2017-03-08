941ad654e7fc5bf2f638effc101723a3dd8cd16c
Jason Amerine <amerine.19@gmail.com>
2017-03-08 14:58:23 -0500

    changed sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,23 +1,7 @@
-require 'sidekiq_calculations'
-require 'csv'
-require 'tempfile'
-
-Sidekiq.configure_client do |config|
-  sidekiq_calculations = SidekiqCalculations.new
-  sidekiq_calculations.raise_error_for_env!
-
-  config.redis = {
-    url: ENV['REDISCLOUD_URL'],
-    size: sidekiq_calculations.client_redis_size
-  }
-end
-
-Sidekiq.configure_server do |config|
-  sidekiq_calculations = SidekiqCalculations.new
-  sidekiq_calculations.raise_error_for_env!
-
-  config.options[:concurrency] = sidekiq_calculations.server_concurrency_size
-  config.redis = {
-    url: ENV['REDISCLOUD_URL']
-  }
-end
++Sidekiq.configure_server do |config|
+ +  config.redis = { url: 'redis://rediscloud:ZoSDUSTr6vCHmj5I@redis-13606.c11.us-east-1-2.ec2.cloud.redislabs.com:13606' }
+ +end
+ +
+ +Sidekiq.configure_client do |config|
+ +  config.redis = { url: 'redis://rediscloud:ZoSDUSTr6vCHmj5I@redis-13606.c11.us-east-1-2.ec2.cloud.redislabs.com:13606' }
+ +end
