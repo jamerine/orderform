e03714bacdf4efddc24f271ce491cff52f90312d
Jason Amerine <amerine.19@gmail.com>
2017-03-09 11:44:59 -0500

    issues

--- app/workers/order_worker.rb
+++ app/workers/order_worker.rb
@@ -3,10 +3,9 @@ class OrderWorker
 
   sidekiq_options queue: :order
 
-  def perform(account_id, order_id, view_context)
+  def perform(account_id, order_id)
     @account = Account.find(account_id)
     @order = Order.find(order_id)
-    @view = view_context
     pdf = OrderInvoicePdf.new(@account, @order)
     OrderMailer.order_email(@account.id, @order.id, pdf).deliver
     InvoiceMailer.invoice_email(@account.id, @order.id, pdf).deliver
