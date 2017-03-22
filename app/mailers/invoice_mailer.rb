cd9c8b9e8517b34feb5545447d03cbfd8a15280d
Jason Amerine <amerine.19@gmail.com>
2017-03-22 16:32:12 -0400

    change from on emails

--- app/mailers/invoice_mailer.rb
+++ app/mailers/invoice_mailer.rb
@@ -1,12 +1,11 @@
 class InvoiceMailer < ApplicationMailer
-  default :from => "jason@dittoh.com"
 
   def invoice_email(account_id, order_id, pdf)
     @account = Account.find(account_id)
     @order = Order.find(order_id)
     pdf = OrderInvoicePdf.new(@account, @order, view_context)
     attachments["order_invoice.pdf"] = { :mime_type => 'application/pdf', :content => pdf.render }
-    mail(:to => @account.email_address, :subject => "New Bulk Order")
+    mail(:to => @account.email_address, :subject => "New Bulk Order", :from => @account.email_address)
   end
 
 end
--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,12 +1,11 @@
 class OrderMailer < ApplicationMailer
-  default :from => "jason@dittoh.com"
 
   def order_email(account_id, order_id, pdf)
     @account = Account.find(account_id)
     @order = Order.find(order_id)
     pdf = OrderInvoicePdf.new(@account, @order, view_context)
     attachments["order_invoice.pdf"] = { :mime_type => 'application/pdf', :content => pdf.render }
-    mail(:to => @order.shipping_email, :subject => "#{ @account.business_name } Bulk Order")
+    mail(:to => @order.shipping_email, :subject => "#{ @account.business_name } Bulk Order", :from => @account.email_address)
   end
 
 end
