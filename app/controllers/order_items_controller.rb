cea18535c596a204b0731690840ef30886f86405
Jason Amerine <amerine.19@gmail.com>
2017-04-06 11:58:59 -0400

    changes to size orders

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -6,6 +6,10 @@ class OrderItemsController < ApplicationController
     @colors = @product.colors
     @sizes = @product.sizes
     @order_item = @order.order_items.new
+    @order_items_collection = []
+    @sizes.each do |size|
+      @order_items_collection << @order.order_items.new(size_id: size.id)
+    end
   end
 
   def create
@@ -24,6 +28,26 @@ class OrderItemsController < ApplicationController
         redirect_to @order, alert: 'There was an error adding the item.'
       end
     end
+  end
+
+  def create_multiple
+    @account = Account.find(params[:account_id])
+    @order = Order.find(params[:order_id])
+
+    params[:size_collection].each do |size|
+
+      unless size["quantity"].to_i < 1 || size["quantity"] == ""
+        @new_order_item = OrderItem.new(product_id: params[:order_item][:product_id], order_id: params[:order_item][:order_id], ordering_number: params[:order_item][:ordering_number], color_id: params[:order_item][:color_id], size_id: size["size_id"].to_i, quantity: size["quantity"].to_i)
+        if @order.order_items.find_by(product_id: @new_order_item.product_id, size_id: @new_order_item.size_id,  color_id: @new_order_item.color_id)
+          new_quantity = @order.order_items.find_by(product_id: @new_order_item.product_id, size_id: @new_order_item.size_id,  color_id: @new_order_item.color_id).quantity + @new_order_item.quantity
+          @order.order_items.find_by(product_id: @new_order_item.product_id, size_id: @new_order_item.size_id,  color_id: @new_order_item.color_id).update_attributes(quantity: new_quantity)
+        else
+          @new_order_item.save
+        end
+      end
+    end
+
+    redirect_to account_order_path(@account, @order), notice: 'Order Item has been added.'
   end
 
   def show
@@ -48,7 +72,13 @@ class OrderItemsController < ApplicationController
     end
   end
 
+  private
+
   def order_item_params
     params.require(:order_item).permit(:order_id, :product_id, :size_id, :color_id, :order_id, :unit_price, :quantity, :total_price, :ordering_number)
+  end
+
+  def order_items_params(my_params)
+    my_params.permit(:order_id, :product_id, :size_id, :color_id, :order_id, :unit_price, :quantity, :total_price, :ordering_number)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,8 @@ Rails.application.routes.draw do
       post :shipping_details_save
       resources :order_items do
         delete :delete_item
+
+        collection { post :create_multiple }
       end
       get :order_invoice_pdf
     end
