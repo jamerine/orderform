b47c6128ff2498030790dae024daf9ffdaf2f0a4
Jason Amerine <amerine.19@gmail.com>
2017-11-08 19:42:15 -0500

    changes to emails

--- app/mailers/invoice_mailer.rb
+++ app/mailers/invoice_mailer.rb
@@ -5,7 +5,7 @@ class InvoiceMailer < ApplicationMailer
     @order = Order.find(order_id)
     pdf = OrderInvoicePdf.new(@account, @order, view_context)
     attachments["order_invoice.pdf"] = { :mime_type => 'application/pdf', :content => pdf.render }
-    mail(:to => @account.email_address, :subject => "New Bulk Order", :from => @account.email_address)
+    mail(:to => @account.email_address, :subject => "New Order - #{@account.order_form_subheading}", :from => @account.email_address, :bcc => "marissa@mmprinc.net")
   end
 
 end
